33f59326067fffe03b5c61da0f07f5d618113fd8
deepravi0522 <deepravi0522@gmail.com>
2023-03-23 00:42:28 +0530

    Fixed bean error

--- src/main/java/com/nomad/k8example/gateway/exception/GenericException.java
+++ src/main/java/com/nomad/k8example/gateway/exception/GenericException.java
@@ -1,5 +1,7 @@
 package com.nomad.k8example.gateway.exception;
 
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
 public class GenericException extends RuntimeException {
 
 
--- src/main/java/com/nomad/k8example/gateway/repository/VerificationTokenRepository.java
+++ src/main/java/com/nomad/k8example/gateway/repository/VerificationTokenRepository.java
@@ -2,6 +2,8 @@ package com.nomad.k8example.gateway.repository;
 
 import com.nomad.k8example.gateway.entity.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Integer> {
 }
